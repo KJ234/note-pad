07165d5e69cc1fb09af3d6101aaa8d63cbab0bfe
KJ234 <KOMALJAVED308@GMAIL.COM>
2022-03-11 17:47:39 +0000

    inital commit testing

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require("path");
 const fs = require("fs");
 
 const app = express();
-const PORT = process.env.PORT || 3002;
+const PORT = process.env.PORT || 3001;
 
 app.use(express.static("public"));
 
