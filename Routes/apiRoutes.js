15184a9b4a51c8ad619a22adc840acb1d7bbc5cb
KJ234 <KOMALJAVED308@GMAIL.COM>
2022-03-12 18:39:16 +0000

    completing

--- Routes/apiRoutes.js
+++ Routes/apiRoutes.js
@@ -7,6 +7,10 @@ const fs = require("fs");
 const uniqid = require("uniqid");
 // GET request for reviews
 Router.get("/api/notes", (req, res) => {
+  fs.readFile("db/db.json", "utf8", (err, data) => {
+    if (err) throw err;
+    return res.json(JSON.parse(data));
+  });
+});
 
-    
 module.exports = Router;
