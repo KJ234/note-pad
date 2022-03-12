f2cc2f76b090a1c0bc232ab25ce5989dcc51345d
KJ234 <KOMALJAVED308@GMAIL.COM>
2022-03-12 13:51:01 +0000

    router.get for another file

--- Routes/htmlRoutes.js
+++ Routes/htmlRoutes.js
@@ -4,3 +4,9 @@ const path = require("path");
 Router.get("/notes", (req, res) => {
   res.sendFile(path.join(__dirname, "./public/notes.html"));
 });
+
+Router.get("/index", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+
+module.exports = Router;
